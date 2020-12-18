bc52a7d1b675abd57e79522a915c9509684fb98c
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2020-12-18 18:12:33 +0600

    Video model and migration added

---
+++ app/Models/Video.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Video extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'runtime',
+        'year',
+        'imdb_id',
+        'imdb_rating',
+        'genres',
+        'country',
+        'poster',
+        'path',
+        'type',
+    ];
+
+    /*
+     * Create slug automatically when video title given
+     */
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($video){
+            $video->slug = Str::slug($video->title.'-'.$video->year);
+        });
+    }
+}
---
+++ database/migrations/2020_12_18_115059_create_videos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->string('runtime', 3);
+            $table->string('year', 4);
+            $table->string('imdb_id',9)->unique();
+            $table->float('imdb_rating', 2, 1);
+            $table->string('genres');
+            $table->string('country', 20);
+            $table->string('poster');
+            $table->string('path');
+            $table->string('type', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
