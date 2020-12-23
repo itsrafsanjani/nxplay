7e068f9ba632b606b77a26c414c157f31d60e1ab
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2020-12-24 00:25:11 +0600

    users.edit and users.destroy completed

--- resources/views/backend/video/index.blade.php
+++ resources/views/backend/video/index.blade.php
@@ -39,6 +39,13 @@
                     </div>
                     <a href="{{ route('videos.create') }}" class="main__title-link">add item</a>
                 </div>
+
+                @if(session()->has('message'))
+                    <div class="alert alert-{{ session('type')}}">
+                        {{ session('message') }}
+                    </div>
+                @endif
+
             </div>
             <!-- end main title -->
 
@@ -94,7 +101,7 @@
                             </td>
                             <td>
                                 <div class="main__table-btns">
-                                    <a href="#modal-status" class="main__table-btn main__table-btn--banned open-modal">
+                                    <a href="#" class="main__table-btn main__table-btn--banned">
                                         <i class="icon ion-ios-lock"></i>
                                     </a>
                                     <a href="#" class="main__table-btn main__table-btn--view">
@@ -104,43 +111,15 @@
                                         <i class="icon ion-ios-create"></i>
                                     </a>
 
-                                    <form action="{{ route('videos.destroy', $video->id) }}" id="deleteForm" class="d-none"
-                                          method="POST" onsubmit="return confirm('Are you sure to delete data?')">
+                                    <form action="{{ route('videos.destroy', $video->id) }}" method="POST" onsubmit="return confirm('Are you sure to delete data?')">
                                         @csrf
                                         @method('DELETE')
+                                        <button type="submit" class="main__table-btn main__table-btn--delete">
+                                            <i class="icon ion-ios-trash"></i>
+                                        </button>
                                     </form>
 
-                                    <a href="#modal-delete" class="main__table-btn main__table-btn--delete open-modal">
-                                        <i class="icon ion-ios-trash"></i>
-                                    </a>
                                 </div>
-
-                                <!-- modal status -->
-                                <div id="modal-status" class="zoom-anim-dialog mfp-hide modal">
-                                    <h6 class="modal__title">Status change</h6>
-
-                                    <p class="modal__text">Are you sure about immediately change status?</p>
-
-                                    <div class="modal__btns">
-                                        <button class="modal__btn modal__btn--apply" type="button">Apply</button>
-                                        <button class="modal__btn modal__btn--dismiss" type="button">Dismiss</button>
-                                    </div>
-                                </div>
-                                <!-- end modal status -->
-
-                                <!-- modal delete -->
-                                <div id="modal-delete" class="zoom-anim-dialog mfp-hide modal">
-                                    <h6 class="modal__title">Item delete</h6>
-
-                                    <p class="modal__text">Are you sure to permanently delete this item?</p>
-
-                                    <div class="modal__btns">
-                                        <button class="modal__btn modal__btn--apply" type="button" onclick="event.preventDefault(); document.getElementById('deleteForm').submit();">Delete</button>
-                                        <button class="modal__btn modal__btn--dismiss" type="button">Dismiss</button>
-                                    </div>
-                                </div>
-                                <!-- end modal delete -->
-
                             </td>
                         </tr>
 
@@ -160,6 +139,7 @@
                     @else
                         <span>{{ $videos->total() }} from {{ $videos->total() }}</span>
                     @endif
+                    {{ $videos->links('backend.bulma') }}
                 </div>
             </div>
             <!-- end paginator -->
