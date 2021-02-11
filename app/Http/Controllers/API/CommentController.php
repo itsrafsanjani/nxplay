3719823b042bb43213a08383e21761e49ad9a163
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-02-11 17:58:41 +0600

    API Comment Updated With Eloquent Relations

--- app/Http/Controllers/API/CommentController.php
+++ app/Http/Controllers/API/CommentController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Comment;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
@@ -20,11 +19,14 @@ class CommentController extends Controller
     {
         $video_id = $request->video_id;
 
-        $comments = DB::table('comments')
+        $comments = Comment::
+        with('replies:id,user_id,video_id,comment_text,parent_id,created_at',
+            'user:id,name,avatar', 'replies.user:id,name,avatar',
+            'commentLikes:id,comment_id,user_id,status', 'replies.commentLikes:id,comment_id,user_id,status',
+            'commentDislikes:id,comment_id,user_id,status', 'replies.commentDislikes:id,comment_id,user_id,status')
             ->where('video_id', '=', $video_id)
-            ->join('users', 'users.id', '=', 'comments.user_id')
-            ->select('comments.id', 'users.name', 'users.email', 'users.avatar', 'comments.comment_text', 'comments.parent_id', 'comments.created_at', 'comments.updated_at')
-            ->orderBy('comments.created_at', 'desc')
+            ->select('id', 'user_id', 'video_id', 'comment_text', 'parent_id', 'created_at')
+            ->latest()
             ->paginate(20);
 
         return response()->json($comments, 200);
@@ -33,7 +35,7 @@ class CommentController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
@@ -45,7 +47,7 @@ class CommentController extends Controller
 
         $validator = Validator::make($request->all(), $rules);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
@@ -67,7 +69,7 @@ class CommentController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -78,8 +80,8 @@ class CommentController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -90,7 +92,7 @@ class CommentController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
