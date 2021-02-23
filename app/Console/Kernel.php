2a03aed51b9c5480b5ccf37e8763023cff3989f9
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-02-23 15:00:13 +0600

    queue:work Added to Console/Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,12 +20,18 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('email:inactive-users')->dailyAt('22:00');
+        $schedule->command('email:inactive-users')
+            ->dailyAt('22:00')
+            ->withoutOverlapping();
+
+        $schedule->command('queue:work --tries=3')
+            ->cron('* * * * *')
+            ->withoutOverlapping();
     }
 
     /**
@@ -35,7 +41,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
