217d4516bbd3ca706dd4eb63b73310bd4c3911a7
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-03-10 13:13:08 +0600

    Installed Laravel IDE Helper Package

---
+++ _ide_helper_models.php
@@ -0,0 +1,262 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Comment
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $video_id
+ * @property string $comment_text
+ * @property int|null $parent_id
+ * @property int|null $replied_to_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\CommentLike[] $commentDislikes
+ * @property-read int|null $comment_dislikes_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\CommentLike[] $commentLikes
+ * @property-read int|null $comment_likes_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|Comment[] $replies
+ * @property-read int|null $replies_count
+ * @property-read \App\Models\User $user
+ * @property-read \App\Models\Video $video
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Comment onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment whereCommentText($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment whereParentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment whereRepliedToId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment whereVideoId($value)
+ * @method static \Illuminate\Database\Query\Builder|Comment withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Comment withoutTrashed()
+ */
+	class Comment extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\CommentLike
+ *
+ * @property int $id
+ * @property int $comment_id
+ * @property int $user_id
+ * @property int|null $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $user
+ * @property-read \App\Models\Video $video
+ * @method static \Illuminate\Database\Eloquent\Builder|CommentLike newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|CommentLike newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|CommentLike query()
+ * @method static \Illuminate\Database\Eloquent\Builder|CommentLike whereCommentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CommentLike whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CommentLike whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CommentLike whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CommentLike whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CommentLike whereUserId($value)
+ */
+	class CommentLike extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Review
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $video_id
+ * @property string $title
+ * @property string $body
+ * @property string $rating
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \App\Models\User $user
+ * @property-read \App\Models\Video $video
+ * @method static \Illuminate\Database\Eloquent\Builder|Review newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Review newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Review onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Review query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereRating($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Review whereVideoId($value)
+ * @method static \Illuminate\Database\Query\Builder|Review withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Review withoutTrashed()
+ */
+	class Review extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string|null $password
+ * @property string|null $provider_id
+ * @property string|null $avatar
+ * @property int $role
+ * @property string|null $fcm_token
+ * @property \Illuminate\Support\Carbon|null $last_login_at
+ * @property string|null $last_login_ip
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\CommentLike[] $commentDislikes
+ * @property-read int|null $comment_dislikes_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\CommentLike[] $commentLikes
+ * @property-read int|null $comment_likes_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Comment[] $comments
+ * @property-read int|null $comments_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Review[] $reviews
+ * @property-read int|null $reviews_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\VideoLike[] $videoLikes
+ * @property-read int|null $video_likes_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Video[] $videos
+ * @property-read int|null $videos_count
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Query\Builder|User onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFcmToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastLoginAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastLoginIp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereProviderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRole($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|User withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|User withoutTrashed()
+ */
+	class User extends \Eloquent implements \Tymon\JWTAuth\Contracts\JWTSubject {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Video
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $title
+ * @property string $slug
+ * @property string $description
+ * @property int $views
+ * @property string $runtime
+ * @property string $year
+ * @property string $imdb_id
+ * @property string $imdb_rating
+ * @property string $genres
+ * @property string $country
+ * @property string $type
+ * @property string $directors
+ * @property string $actors
+ * @property string|null $box_office
+ * @property string $poster
+ * @property string $video
+ * @property string $photos
+ * @property string $age_rating
+ * @property int $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Comment[] $comments
+ * @property-read int|null $comments_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Comment[] $replies
+ * @property-read int|null $replies_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Review[] $reviews
+ * @property-read int|null $reviews_count
+ * @property-read \App\Models\User $user
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\VideoLike[] $videoDislikes
+ * @property-read int|null $video_dislikes_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\VideoLike[] $videoLikes
+ * @property-read int|null $video_likes_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Video newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Video newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Video query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereActors($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereAgeRating($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereBoxOffice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereCountry($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereDirectors($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereGenres($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereImdbId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereImdbRating($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video wherePhotos($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video wherePoster($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereRuntime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereVideo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereViews($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Video whereYear($value)
+ */
+	class Video extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\VideoLike
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $video_id
+ * @property int|null $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $user
+ * @property-read \App\Models\Video $video
+ * @method static \Illuminate\Database\Eloquent\Builder|VideoLike newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|VideoLike newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|VideoLike query()
+ * @method static \Illuminate\Database\Eloquent\Builder|VideoLike whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|VideoLike whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|VideoLike whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|VideoLike whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|VideoLike whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|VideoLike whereVideoId($value)
+ */
+	class VideoLike extends \Eloquent {}
+}
+
