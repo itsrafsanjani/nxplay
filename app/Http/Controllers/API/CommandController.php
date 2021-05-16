d0c2b23bc9d5b3f045b5cdd9e84196e0439ecdfa
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-05-16 12:35:51 +0600

    API Command Security Updated Using API Command Config

--- app/Http/Controllers/API/CommandController.php
+++ app/Http/Controllers/API/CommandController.php
@@ -8,30 +8,52 @@ use Illuminate\Support\Facades\Artisan;
 
 class CommandController extends Controller
 {
-    public function clearCache(Request $request): \Illuminate\Http\JsonResponse
+    public function checkAccess($request): bool
     {
         $username = $request->username;
         $password = $request->password;
 
-        if($username == "nxplay" && $password == "AreyHogaMarseRe!"){
-            Artisan::call('optimize:clear');
-
-            return response()->json(['message' => 'Command executed successfully!']);
+        if ($username == config('api_command.username') && $password == config('api_command.password')) {
+            return true;
         }
 
-        return response()->json(['message' => 'username, password mone nai abar command maraite aicho!']);
+        return false;
+    }
+
+    public function clearCache(Request $request): \Illuminate\Http\JsonResponse
+    {
+        if ($this->checkAccess($request)) {
+            Artisan::call('optimize:clear');
+
+            return response()->json([
+                'success' => true,
+                'status_code' => 200,
+                'message' => 'Command executed successfully!'
+            ]);
+        }
+
+        return response()->json([
+            'success' => false,
+            'status_code' => 403,
+            'message' => 'username, password does not match!'
+        ], 401);
     }
 
     public function refreshSeed(Request $request): \Illuminate\Http\JsonResponse
     {
-        $username = $request->username;
-        $password = $request->password;
-
-        if($username == "nxplay" && $password == "AreyHogaMarseRe!"){
+        if ($this->checkAccess($request)) {
             Artisan::call('migrate:refresh --seed');
-            return response()->json(['message' => 'migrate:refresh --seed command executed successfully!']);
+            return response()->json([
+                'success' => true,
+                'status_code' => 200,
+                'message' => 'migrate:refresh --seed command executed successfully!'
+            ]);
         }
 
-        return response()->json(['message' => 'username, password mone nai abar command maraite aicho!']);
+        return response()->json([
+            'success' => false,
+            'status_code' => 403,
+            'message' => 'username, password does not match!'
+        ], 401);
     }
 }
---
+++ config/api_command.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'username' => env('API_COMMAND_USERNAME'),
+    'password' => env('API_COMMAND_PASSWORD'),
+];
