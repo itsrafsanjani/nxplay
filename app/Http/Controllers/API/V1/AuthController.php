395e8fb66ba662e13a80f7ab314de070af1f84f6
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-08-22 23:03:13 +0600

    feat: 'api_v1' route file updated with laravel 8 style route

--- app/Http/Controllers/API/V1/AuthController.php
+++ app/Http/Controllers/API/V1/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,39 +1,50 @@
 <?php
 
-Route::group(['namespace' => 'App\Http\Controllers\API', 'prefix' => 'v1'], function () {
-    Route::post('login', 'AuthController@login');
-    Route::post('register', 'AuthController@register');
+use App\Http\Controllers\API\CommandController;
+use App\Http\Controllers\API\CommentController;
+use App\Http\Controllers\API\CommentLikeController;
+use App\Http\Controllers\API\HomeController;
+use App\Http\Controllers\API\NotificationController;
+use App\Http\Controllers\API\ReviewController;
+use App\Http\Controllers\API\SearchController;
+use App\Http\Controllers\API\V1\AuthController;
+use App\Http\Controllers\API\VideoController;
+use App\Http\Controllers\API\VideoLikeController;
 
-    Route::post('forgot-password', 'AuthController@forgotPassword');
-    Route::post('reset', 'AuthController@passwordReset');
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
 
-    Route::get('refresh', 'AuthController@refresh');
+    Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
+    Route::post('reset', [AuthController::class, 'passwordReset']);
 
-    Route::post('google', 'AuthController@google');
-    Route::post('github', 'AuthController@github');
-    Route::post('facebook', 'AuthController@facebook');
+    Route::get('refresh', [AuthController::class, 'refresh']);
 
-    Route::apiResource('home', 'HomeController', ['as' => 'api.v1'])
+    Route::post('google', [AuthController::class, 'google']);
+    Route::post('github', [AuthController::class, 'github']);
+    Route::post('facebook', [AuthController::class, 'facebook']);
+
+    Route::apiResource('home', HomeController::class, ['as' => 'api.v1'])
         ->only('index');
 
     /**
      * Clear cache, route, config, view from command using any Rest API client
      * You have to send username and password for security reason.
      */
-    Route::get('command/clear-cache', 'CommandController@clearCache');
-    Route::get('command/refresh-seed', 'CommandController@refreshSeed');
+    Route::get('command/clear-cache', [CommandController::class, 'clearCache']);
+    Route::get('command/refresh-seed', [CommandController::class, 'refreshSeed']);
 
     Route::group(['middleware' => 'jwt'], function () {
-        Route::post('logout', 'AuthController@logout');
-        Route::get('me', 'AuthController@me');
-        Route::patch('users/{user}', 'AuthController@update');
-        Route::apiResource('videos', 'VideoController', ['as' => 'api.v1'])
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('me', [AuthController::class, 'me']);
+        Route::patch('users/{user}', [AuthController::class, 'update']);
+        Route::apiResource('videos', VideoController::class, ['as' => 'api.v1'])
             ->only('index', 'show');
-        Route::apiResource('comments', 'CommentController', ['as' => 'api.v1']);
-        Route::post('videos/like', 'VideoLikeController@likeOrDislike');
-        Route::apiResource('reviews', 'ReviewController', ['as' => 'api.v1']);
-        Route::post('comments/like', 'CommentLikeController@commentLikeOrDislike');
-        Route::get('search', 'SearchController@index');
-        Route::get('notifications', 'NotificationController@index');
+        Route::apiResource('comments', CommentController::class, ['as' => 'api.v1']);
+        Route::post('videos/like', [VideoLikeController::class, 'likeOrDislike']);
+        Route::apiResource('reviews', ReviewController::class, ['as' => 'api.v1']);
+        Route::post('comments/like', [CommentLikeController::class, 'commentLikeOrDislike']);
+        Route::get('search', [SearchController::class, 'index']);
+        Route::get('notifications', [NotificationController::class, 'index']);
     });
 });
